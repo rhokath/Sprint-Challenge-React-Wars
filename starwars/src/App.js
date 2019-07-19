a8070f1bb5820787394b0ac5e5ffa781eb705a51
Kathryn Rhoads <46656679+rhokath@users.noreply.github.com>
2019-07-19 12:24:17 -0400

    characters are on page

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 import './App.css';
+import PersonCard from "./components/PersonCard";
+import PeopleGrid from "./components/PersonCard";
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [data, setData] = useState([]);
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  useEffect(()=>{
+    axios.get("https://swapi.co/api/people/")
+    .then(res => {
+      console.log("this is my response", res)
+      console.log(res.data.results)
+      const starwarspeople = res.data.results
+      setData(starwarspeople);
+    })
+    .catch(error => {
+      console.log("this is an error", error)
+    })
 
+
+  }, [])
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      {data.map( (item, index) => {
+        console.log(item)
+        return <PersonCard key={index}
+         name={item.name} 
+         bday={item.birth_year}
+         gender={item.gender}
+         hair={item.hair_color}
+         height={item.height}
+         mass={item.mass}
+         />
+      })}
     </div>
   );
 }
